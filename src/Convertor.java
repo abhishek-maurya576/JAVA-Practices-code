f6dcdc2080e1516640b9ca072d448b7611c77d3b
Abhishek Maurya <maurya972137@gmail.com>
2024-07-18 17:48:23 +0530

    This is my Java Practices code

--- src/Convertor.java
+++ src/Convertor.java
@@ -1,5 +1,15 @@
+
+import java.util.Scanner;
+
 public class Convertor {
     public static void main(String[] args) {
-
+    //celsius to fahrenheit
+        System.out.print("Enter the temperature: ");
+        Scanner scan = new Scanner(System.in);
+        float temp = scan.nextInt();
+        temp = temp*9;
+        temp = temp/5;
+        temp = temp + 32;
+        System.out.println("Temperature in fahrenheit: "+temp + "f");
     }
 }
