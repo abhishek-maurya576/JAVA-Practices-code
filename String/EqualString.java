b2302ab326f281a1556ee0c31993d7a32afc747c
Abhishek <87256096+abhishek-maurya576@users.noreply.github.com>
2024-11-08 23:40:30 +0530

    Update EqualString.java

--- String/EqualString.java
+++ String/EqualString.java
@@ -1,13 +1,15 @@
 public class EqualString {
     public static void main(String[] args) {
         String str = "Hello";
-        String str2 = "Hello";
-        String str3 = "HELLO";
-        String str1 = new String("Hello");
-        System.out.println(str1.equals(str2));
-        System.out.println(str.equals(str2));
+        String str1 = "Hello";
+        String str2 = "HELLO";
+        String str3 = new String("Hello");
+        System.out.println(str.equals(str1));
         System.out.println(str.equals(str3));
-        System.out.println(str.equalsIgnoreCase(str3));
+        System.out.println(str.equals(str2));
+        System.out.println(str.equalsIgnoreCase(str2));
+        System.out.println(str==str3); //false why?
+        System.out.println(str==str1);
         
     }
 }
